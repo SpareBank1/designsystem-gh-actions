07a243485dd4e29104204ba97a4212f66429c156
Steffen Martinsen <steffen@steffenmedia.no>
2020-02-21 14:16:44 +0100

    Test simple action switcher

--- publish-to-npm/src/index.ts
+++ publish-to-npm/src/index.ts
@@ -1,24 +1,41 @@
 import { createErrorMessage, createChangelogMessage, postSlackMessage } from './lib/slack';
 import { getChangedPackages, publishChangedPackages } from './lib/lerna';
+import * as core from '@actions/core';
 
-const packages = getChangedPackages();
+const action = core.getInput('action', { required: true});
 
-if(packages && packages.length > 0){
-    
-    const publish = publishChangedPackages();
+switch(action){
+    case 'lerna-changed':
+        console.log('lerna changed');
+        break;
 
-    if(publish.success){
-        postSlackMessage(
-            createChangelogMessage(packages)
-            // @to-do: merge 
-        );
-    } else {
-        postSlackMessage(
-            createErrorMessage(
-                `:boom: An error occured during publishing to NPM.`,
-                publish.error,
-            )
-        );
-    }
+    case 'lerna-publish':
+        console.log('run publish action');
+        break;
 
+    default:
+        console.log('unknown param');
+        break;
 }
+
+// const packages = getChangedPackages();
+
+// if(packages && packages.length > 0){
+    
+//     const publish = publishChangedPackages();
+
+//     if(publish.success){
+//         postSlackMessage(
+//             createChangelogMessage(packages)
+//             // @to-do: merge 
+//         );
+//     } else {
+//         postSlackMessage(
+//             createErrorMessage(
+//                 `:boom: An error occured during publishing to NPM.`,
+//                 publish.error,
+//             )
+//         );
+//     }
+
+// }
